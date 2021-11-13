4ddc71d1f080995de42fb08f8e8adb6c2e5bd1b3
aryan anwar <72667005+aryananwar@users.noreply.github.com>
2021-11-12 20:27:11 -0500

    inital commit
    looks thru json file of palace items, check if item is available, no autocheckout functionality yet

---
+++ palace.py
@@ -0,0 +1,32 @@
+import requests
+
+class Selection:
+    def __init__(self, item, size, amount):
+        self.item = item
+        self.size = size
+        self.amount = amount
+
+def printItems(products):
+    for product in products['products']:
+        for variant in product['variants']:
+            print(product['title'] + " " + variant['option1'] + " " + str(variant['available']))
+       
+
+def findItem(item, size, products):
+    for product in products['products']:
+        for variant in product['variants']:
+            if (product['title'] == item and variant['option1'].lower() == size.lower() and variant['available'] == True):
+                print("Item is available")
+                return True
+    return False
+
+def main():
+    s1 = Selection('YE OLDE HOOD WHITE', 'Small', 1)
+    r = requests.get('https://shop-usa.palaceskateboards.com/products.json')
+    products = r.json()
+    #printItems(products)
+    findItem(s1.item, s1.size, products)
+
+
+main()
+
